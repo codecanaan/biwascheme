032c7c6a27b81f5661638425e558ed0abcb74766
lyhcode (Yan-hong Lin) <lyhcode@gmail.com>
2013-01-17 17:21:23 +0800

    add bs-console-plain mode

--- src/platforms/browser/console.js
+++ src/platforms/browser/console.js
@@ -10,6 +10,11 @@ Console.puts = function(str, no_newline) {
 	span.html(text.replace(/\n/g,"<br>").replace(/ /g,"&nbsp;"));
 	console.append(span);
   }
+  console = $('#bs-console-plain');
+  if (console[0]) {
+	text = _.escape(str + (no_newline ? "" : "\n"));
+	console.append(text);
+  }
 };
 
 Console.p = function (/*ARGS*/){
